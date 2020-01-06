bc01b1e6a5f772c41e9c168ef531d522b7f70fc4
Marcos Soares <slipmp@gmail.com>
2020-01-05 17:31:11 -0800

    Updating dependencies to their latest versions

--- src/PhotoApp.js
+++ src/PhotoApp.js
@@ -15,40 +15,40 @@ such as having Local state, a feature available only to classes.
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './PhotoApp.css';
-import axios from 'axios'
+import axios from 'axios';
 
 
-const PhotoApp = () =>  {
-  const [listOfPhotos, setListOfPhotos] = useState({ listOfPhotos: [] });
+const PhotoApp = () => {
+    const [listOfPhotos, setListOfPhotos] = useState({ listOfPhotos: [] });
 
-  //Go and featch a few photos belonging to the album 1 using axios
-  useEffect(() => {
-    async function fetchData() {
-      const result = await axios(
-        'https://jsonplaceholder.typicode.com/photos?albumId=1',
-      );
-      console.log(result.data);
-      console.log('Photos have been fetched and set into the state property called listOfPhotos');
-      setListOfPhotos(result.data);
-    }
-    fetchData();
-  }, []);
+    //Go and featch a few photos belonging to the album 1 using axios
+    useEffect(() => {
+        async function fetchData() {
+            const result = await axios(
+                'https://jsonplaceholder.typicode.com/photos?albumId=1',
+            );
+            console.log(result.data);
+            console.log('Photos have been fetched and set into the state property called listOfPhotos');
+            setListOfPhotos(result.data);
+        }
+        fetchData();
+    }, []);
 
-  return (
-    <div className="PhotoApp">
-      <header className="PhotoApp-header">
-        <img src={logo} className="PhotoApp-logo" alt="logo" />
-        <h1 className="PhotoApp-title">Photos Application</h1>
-        <h4>My first React Application to understand the basics. <br />
-        PhotoApp fetches photos from a public accessible API and renders on the page</h4>
-      </header>
-      <p className="PhotoApp-intro">
-        List of Photos
-      </p>
-      <PhotoList photos={listOfPhotos}></PhotoList>  
-    </div>
-  );
-}
+    return (
+        <div className="PhotoApp">
+            <header className="PhotoApp-header">
+                <img src={logo} className="PhotoApp-logo" alt="logo" />
+                <h1 className="PhotoApp-title">Photos Application</h1>
+                <h4>My first React Application to understand the basics. <br />
+                    PhotoApp fetches photos from a public accessible API and renders on the page</h4>
+            </header>
+            <p className="PhotoApp-intro">
+                List of Photos
+            </p>
+            <PhotoList photos={listOfPhotos}></PhotoList>
+        </div>
+    );
+};
 
 const PhotoList = props => ( 
   <>
